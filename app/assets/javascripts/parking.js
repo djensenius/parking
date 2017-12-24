16280939bfd5ce72025916cb34f016cb1bdfb6d5
David Jensenius <djensenius@gmail.com>
2017-12-24 13:21:24 -0500

    Initial design

--- app/assets/javascripts/parking.js
+++ app/assets/javascripts/parking.js
@@ -1,3 +1,11 @@
 // @flow
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
