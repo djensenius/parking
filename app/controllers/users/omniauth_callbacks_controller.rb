7d9b9bcf08cbfcef44e78ec80d19eab3c29d26de
David Jensenius <djensenius@gmail.com>
2018-02-10 21:36:36 -0500

    Get a step closer to signing in with Twitter

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
-class Users
+module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You should configure your model like this:
     devise :omniauthable, omniauth_providers: [:twitter]
 
     # You should also create an action method in this controller like this:
     def twitter
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+        set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
+      else
+        session["devise.twitter_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+    end
+
+    def facebook
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+    end
+
+    def failure
+      redirect_to root_path
     end
 
     # More info at:
