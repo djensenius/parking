550f1b0adb9cf361d488b83c2129bd64a92d103e
David Jensenius <djensenius@gmail.com>
2018-03-27 22:51:33 -0400

    Fix linting problems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ SimpleCov.start
 require "codecov"
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../config/environment", __dir__)
 require "rails/test_help"
 require "minitest/rails"
 require "mocha/mini_test"
