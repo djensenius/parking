221fd7f2e60e46df439edab707cd76c8c5e3f8f7
David Jensenius <djensenius@gmail.com>
2018-03-27 23:14:57 -0400

    Fix error because I wasn’t importing CSV

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "csv"
+
 class Parking < ApplicationRecord
   validates :unit, presence: true, numericality: { only_integer: true }
   validates :code, presence: true
