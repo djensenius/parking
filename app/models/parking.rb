e517c91f64ee0ae4bfef6d61bcd42c133d3ef7ca
David Jensenius <djensenius@gmail.com>
2018-10-19 10:32:15 -0400

    Remove code requirement

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -45,7 +45,6 @@ end
 
 class Parking < ApplicationRecord
   validates :unit, presence: true, numericality: { only_integer: true }
-  validates :code, presence: true
   validates :make, presence: true
   validates :color, presence: true
   validates :license, presence: true
